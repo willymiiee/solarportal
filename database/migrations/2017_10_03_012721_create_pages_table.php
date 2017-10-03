013aafa7dc68ffeea5da5ce86ab21cea32d48f6e
Willy Nugraha <willymiiee@gmail.com>
2017-10-03 09:02:28 +0700

    Page model & migration

---
+++ database/migrations/2017_10_03_012721_create_pages_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pages', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->text('content');
+            $table->integer('created_by');
+            $table->integer('updated_by');
+            $table->integer('deleted_by');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pages');
+    }
+}
