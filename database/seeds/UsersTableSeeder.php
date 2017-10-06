a7381b107523e7e1e24a035332bb47d105cefb1b
Willy Nugraha <willymiiee@gmail.com>
2017-10-06 08:55:17 +0700

    seeder for user

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->delete();
+        DB::table('users')->insert([
+            'name' => 'Admin',
+            'email' => 'admin@test.com',
+            'password' => app('hash')->make('12345678')
+        ]);
+    }
+}
