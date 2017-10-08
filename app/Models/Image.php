ef7f9fe420bc7e37038d17a003c1ee5f5746d278
Willy Nugraha <willymiiee@gmail.com>
2017-10-08 17:29:16 +0700

    add image model & migration

---
+++ app/Models/Image.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Image extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'url'
+    ];
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = [
+        'created_at', 'updated_at'
+    ];
+
+    public function pages()
+    {
+        return $this->belongsToMany('App\Models\Page')
+            ->wherePivot('item_type', 'page')
+            ->withTimestamps();
+    }
+}
