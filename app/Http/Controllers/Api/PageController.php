de6759b2fabd51628bc971d8faec783c412513cd
Willy Nugraha <willymiiee@gmail.com>
2017-10-08 17:29:48 +0700

    add PageController for API

---
+++ app/Http/Controllers/Api/PageController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+use App\Models\Page;
+
+class PageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $items = Page::whereNull('deleted_at')->orderBy('created_at', 'desc')->get();
+        return response()->json($items);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $item = Page::where('id', $id)->first();
+        return response()->json($item);
+    }
+}
