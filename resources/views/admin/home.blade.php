80e7275abe9470cf0fbd3f39c6dfef9393e4aa9a
Willy Nugraha <willymiiee@gmail.com>
2017-10-08 17:33:54 +0700

    update home @ admin

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,1 +1,29 @@
 @extends('layouts.admin')
+
+@section('style')
+@endsection
+
+@section('header')
+<h1>
+    Dashboard
+    <small>Control panel</small>
+</h1>
+@endsection
+
+@section('breadcrumb')
+{{--  <ol class="breadcrumb">
+    <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+    <li class="active">Dashboard</li>
+</ol>  --}}
+@endsection
+
+@section('content')
+<!-- Main row -->
+<div class="row">
+    You are logged in!
+</div>
+<!-- /.row (main row) -->
+@endsection
+
+@section('script')
+@endsection
