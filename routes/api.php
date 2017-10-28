ba531da45d54cccb57eade65fdd1cd3493386f7f
Willy Nugraha <willymiiee@gmail.com>
2017-10-29 01:22:37 +0700

    convert api url

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::namespace('Api')->group(function() {
-    Route::apiResource('pages', 'PageController');
-    Route::apiResource('posts', 'PostController');
+    Route::apiResource('articles', 'ArticleController');
 });
