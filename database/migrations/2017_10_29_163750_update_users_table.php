affd7e74f5cdb22018b024f90a025e9c97dca893
Willy Nugraha <willymiiee@gmail.com>
2017-11-04 22:26:00 +0700

    update user migration

---
+++ database/migrations/2017_10_29_163750_update_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->enum('type', ['C', 'V', 'A'])->default('C');
+            $table->string('confirmation_code')->nullable();
+            $table->dateTime('confirmed_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
