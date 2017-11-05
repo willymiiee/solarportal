63245265a531f9a4f1f915103e129639a1a1a1b5
Willy Nugraha <willymiiee@gmail.com>
2017-11-05 09:24:56 +0700

    update header

--- resources/views/includes/frontend/menu.blade.php
+++ resources/views/includes/frontend/menu.blade.php
@@ -2,9 +2,24 @@
     <li>
         <a href="{{ url('') }}">Home</a>
     </li>
+
     @foreach ($menus as $m)
     <li>
         <a href="{{ url($m->slug) }}">{{ $m->title }}</a>
     </li>
     @endforeach
+
+    @if (Auth::check())
+    <li>
+        <b>Hi, {{ Auth::user()->name }}</b>
+    </li>
+    @else
+    <li>
+        <a href="{{ url('login') }}">Login</a>
+    </li>
+    <li><b>/</b></li>
+    <li>
+        <a href="{{ url('register') }}">Register</a>
+    </li>
+    @endif
 </ul>
