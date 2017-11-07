682a9c0bb9e02dc1ef3d3d6086279dc98f57356f
Willy Nugraha <willymiiee@gmail.com>
2017-11-08 00:32:15 +0700

    update menu

--- resources/views/includes/frontend/menu.blade.php
+++ resources/views/includes/frontend/menu.blade.php
@@ -4,22 +4,37 @@
     </li>
 
     @foreach ($menus as $m)
+
     <li>
         <a href="{{ url($m->slug) }}">{{ $m->title }}</a>
     </li>
+
     @endforeach
 
     @if (Auth::check())
-    <li>
-        <b>Hi, {{ Auth::user()->name }}</b>
+
+    <li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+            <b>Hi, {{ Auth::user()->name }}</b>
+            <span class="caret"></span>
+        </a>
+
+        <ul class="dropdown-menu">
+            <li><a href="">Profile</a></li>
+            <li><a href="">Change Password</a></li>
+            <li>
+                <a href="{{ route('logout') }}"
+                    onclick="event.preventDefault();
+                                document.getElementById('logout-form').submit();">
+                    Logout
+                </a>
+
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    {{ csrf_field() }}
+                </form>
+            </li>
+        </ul>
     </li>
-    @else
-    <li>
-        <a href="{{ url('login') }}">Login</a>
-    </li>
-    <li><b>/</b></li>
-    <li>
-        <a href="{{ url('register') }}">Register</a>
-    </li>
+
     @endif
 </ul>
