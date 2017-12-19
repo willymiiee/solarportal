0a42b9dfd18422b8ebff8ae447ac2987b78ccb38
Antoni Putra <akiddcode@gmail.com>
2017-12-19 23:11:34 +0700

    fix error duplication when migration on fresh install. & add migration to add main_domicile column to users table

--- database/migrations/2017_10_29_163750_update_users_table.php
+++ database/migrations/2017_10_29_163750_update_users_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class UpdateUsersTable extends Migration
 {
@@ -17,6 +17,8 @@ class UpdateUsersTable extends Migration
             $table->enum('type', ['C', 'V', 'A'])->default('C');
             $table->string('confirmation_code')->nullable();
             $table->dateTime('confirmed_at')->nullable();
+            $table->string('phone')->nullable();
+            $table->text('address')->nullable();
         });
     }
 
