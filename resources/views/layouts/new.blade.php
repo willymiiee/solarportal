694a2cea568359c90d5bfa3c8cffdafbbb298be4
Willy Nugraha <willymiiee@gmail.com>
2017-12-21 09:04:15 +0700

    update footer

--- resources/views/layouts/new.blade.php
+++ resources/views/layouts/new.blade.php
@@ -23,69 +23,6 @@
     @yield('content')
 
     @include('includes.frontend.footer')
-    <footer class="container-fluid">
-        <div class="row">
-            <div class="col-sm-12 col-md-3 col-lg-3 col-xl-3 footer-col">
-                <div class="footer-col-title">Contact</div>
-
-                <div class="footer-col-content">
-                    <i class="icon icon-location-pin"></i>
-                    <span>Gedung BPPT. Jl. M.H. Thamrin No.8, RT.2/RW.1, Menteng, Kota Jakarta Pusat</span><br>
-                    {{--  <i class="icon icon-phone"></i>
-                    <span>+1 212-431-2100</span><br>  --}}
-                    <i class="icon icon-envelope"></i>
-                    <span>info@gerakannasionalsejutasuryaatap.org</span>
-                </div>
-            </div>
-
-            <div class="col-sm-12 col-md-3 col-lg-3 col-xl-3 footer-col">
-                <div class="footer-col-title">Recent posts</div>
-
-                <div class="footer-col-content">
-                    <ul>
-                        <li>
-                            <i class="icon icon-arrow-right"></i>
-                            <a href="#">Blog Post 4</a>
-                        </li>
-
-                        <li>
-                            <i class="icon icon-arrow-right"></i>
-                            <a href="#">Blog Post 3</a>
-                        </li>
-
-                        <li>
-                            <i class="icon icon-arrow-right"></i>
-                            <a href="#">Blog Post 2</a>
-                        </li>
-
-                        <li>
-                            <i class="icon icon-arrow-right"></i>
-                            <a href="#">Blog Post 1</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-
-            <div class="col-sm-12 col-md-3 col-lg-3 col-xl-3 footer-col">
-                <div class="footer-col-title">About</div>
-
-                <div class="footer-col-content">
-                    <a href="#">Co-coordinator</a>
-                    <a href="#">Participants</a>
-                    {{--  <a href="#">Solar roof benefits</a>  --}}
-                </div>
-            </div>
-
-            <div class="col-sm-12 col-md-3 col-lg-3 col-xl-3 footer-col">
-                <div class="footer-col-title">Search</div>
-
-                <div class="footer-col-content">
-                    <input type="text" id="footerSearch" placeholder="Search...">
-                    <a href="#" id="footerSearchBtn"><i class="icon icon-magnifier"></i></a>
-                </div>
-            </div>
-        </div>
-    </footer>
 
     <!-- Bootstrap core JavaScript
     ================================================== -->
