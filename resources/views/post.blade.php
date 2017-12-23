9ced7522c422c3e69829279fc6bedaa405d13327
Willy Nugraha <willymiiee@gmail.com>
2017-12-23 16:33:07 +0700

    update padding on post page

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -13,7 +13,7 @@
 @endsection
 
 @section('content')
-    <div class="container pt-2">
+    <div class="container pt-2 pb-3">
         <div class="row">
             <div class="col-12 mt-2">
                 <small>
