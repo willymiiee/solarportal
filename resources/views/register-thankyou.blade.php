a46239b84a0c678e6a76f648c9dc34d00d866e43
Willy Nugraha <willymiiee@gmail.com>
2017-12-27 14:35:06 +0700

    update layout for register thankyou page

--- resources/views/register-thankyou.blade.php
+++ resources/views/register-thankyou.blade.php
@@ -1,23 +1,20 @@
-@extends('layouts.main')
+@extends('layouts.new')
 
 @section('menu')
     @include('includes.frontend.menu', ['menus' => $data['menu']])
 @endsection
 
 @section('content')
-    <!--Page Register Thankyou Start-->
-    <section class="tl-properties-section">
-        <div class="container">
-            <div class="row" style="padding: 50px 0">
-                <h2 style="margin-bottom: 30px">Registrasi Sukses!</h2>
-
+    <div class="container pt-2 pb-2">
+        <div class="row">
+            <div class="col-12">
+                <h2>Registrasi Sukses!</h2>
                 <p>Terima kasih sudah mendaftarkan di portal Gerakan Sejuta Surya Atap.</p>
                 <p>Kami sudah mengirimkan e-mail untuk aktivasi akun anda.</p>
                 <p>Mohon periksa e-mail anda dan mengklik tautan untuk aktivasi.</p>
             </div>
         </div>
-    </section>
-    <!--Page Register Thankyou End-->
+    </div>
 @endsection
 
 @section('script')
