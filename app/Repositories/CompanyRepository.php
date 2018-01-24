bd104047c884ff40cf9b61f30809873d163adef6
Antoni Putra <akiddcode@gmail.com>
2018-01-25 05:55:32 +0700

    fix bug on delete company and its services

--- app/Repositories/CompanyRepository.php
+++ app/Repositories/CompanyRepository.php
@@ -82,7 +82,9 @@ class CompanyRepository extends BaseRepository
             // automatically deleted by mysql foreign cascade
             if (count($model->services) > 0) {
                 foreach ($model->services as $key => $serv) {
-                    Storage::delete('public/' . $serv['image']);
+                    if (!empty($serv['image'])) {
+                        Storage::delete('public/' . $serv['image']);
+                    }
 
                     // delete on s3 driver
                     // Storage::disk('s3')->delete('folder_path/file_name.jpg');
