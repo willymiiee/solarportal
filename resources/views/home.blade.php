888f345b388516a9794bbb1b7fb4ef484952ee52
Willy Nugraha <willymiiee@gmail.com>
2018-01-24 20:26:34 +0700

    update str_limit on home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -123,7 +123,7 @@
                 <img src="{{ $b->image ? url($b->image->url) : '' }}" alt="" class="blog-cover">
                 <div class="blog-item-title">{{ $b->title }}</div>
                 <div class="blog-item-content">
-                    {!! substr($b->content, 0, 120).'...' !!}
+                    {!! str_limit($b->content, 150) !!}
                 </div>
                 <a href="{{ url($b->slug) }}" class="blog-item-readmore">Read more</a>
             </div>
