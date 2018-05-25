6cfbb70f7bfb3a2d179053f8bb9f303b83010364
Willy Nugraha <willymiiee@gmail.com>
2018-05-25 11:49:54 +0800

    create QuoteController

---
+++ app/Http/Controllers/Api/QuoteController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Province;
+use App\Models\Regency;
+use App\Models\District;
+use Config;
+
+class QuoteController extends Controller
+{
+    public function postQuote(Request $request)
+    {
+        $data = $request->all();
+        $usePerDay = $data['tagihan'] / Config::get('constants.pln_tld') * 30;
+        $pvRequired = $usePerDay / Config::get('constants.sun_hour');
+        $pvAllowed = min($pvRequired, $data['kapasitas']);
+        $cost = $pvRequired * Config::get('constants.cost_kw');
+        $roofArea = $pvAllowed * Config::get('constants.panel_area') * 1000;
+        $saving = $pvAllowed * Config::get('constants.sun_hour') * Config::get('constants.pln_tld') * 30;
+        return $saving;
+    }
+}
