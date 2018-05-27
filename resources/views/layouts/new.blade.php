5d7d0507c2650da7ca39333c04319b6b523788d1
Willy Nugraha <willymiiee@gmail.com>
2018-05-27 22:00:38 +0800

    add bootstrap js on template

--- resources/views/layouts/new.blade.php
+++ resources/views/layouts/new.blade.php
@@ -47,6 +47,7 @@
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-beta/js/bootstrap.min.js"></script>
     <script src="{{ asset('js/bootstrap4.min.js') }}"></script>
 
     {{-- Sweetalert --}}
