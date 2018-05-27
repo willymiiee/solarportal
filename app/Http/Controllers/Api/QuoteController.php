e9d1cd2cd538fa5084eef9cffc65baacf5a2637e
Willy Nugraha <willymiiee@gmail.com>
2018-05-27 15:59:06 +0800

    update result for Quote

--- app/Http/Controllers/Api/QuoteController.php
+++ app/Http/Controllers/Api/QuoteController.php
@@ -14,12 +14,19 @@ class QuoteController extends Controller
     public function postQuote(Request $request)
     {
         $data = $request->all();
-        $usePerDay = intval(preg_replace('/[^\d.]/', '', $data['tagihan'])) / Config::get('constants.pln_tld') * 30;
+        $usePerDay = number_format(intval(preg_replace('/[^\d.]/', '', $data['tagihan'])) / (Config::get('constants.pln_tld') * 30), 1);
         $pvRequired = $usePerDay / Config::get('constants.sun_hour');
-        $pvAllowed = min($pvRequired, intval(preg_replace('/[^\d.]/', '', $data['kapasitas'])));
+        $pvAllowed = min($pvRequired, $data['kapasitas']/1000);
         $cost = $pvRequired * Config::get('constants.cost_kw');
         $roofArea = $pvAllowed * Config::get('constants.panel_area') * 1000;
         $saving = $pvAllowed * Config::get('constants.sun_hour') * Config::get('constants.pln_tld') * 30;
-        return $saving;
+        return [
+            'use_per_day' => $usePerDay,
+            'pv_required' => $pvRequired * 1000,
+            'pv_allowed' => $pvAllowed * 1000,
+            'cost' => $cost,
+            'roof_area' => $roofArea,
+            'saving' => $saving
+        ];
     }
 }
