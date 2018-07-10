4c5d50afeb94211eed226a995571308bc010aec8
Willy Nugraha <willymiiee@gmail.com>
2018-07-10 10:45:19 +0800

    add constants

--- config/constants.php
+++ config/constants.php
@@ -5,5 +5,7 @@ return [
     'pln_tld' => 1467.3,
     'panel_area' => 1.8/250,
     'area_allowance' => 10/100,
-    'cost_kw' => 15000000
+    'cost_kw' => 15000000,
+    'nett_tdl' => 1.06,
+    'day_share' => 0.2
 ];
