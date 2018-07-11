16f166d90a0f00b9875321f04eec3635eb9e8bf4
Willy Nugraha <willymiiee@gmail.com>
2018-07-11 10:25:31 +0800

    force browser to validate everytime

--- resources/views/layouts/new.blade.php
+++ resources/views/layouts/new.blade.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
+    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+    <meta http-equiv="Pragma" content="no-cache" />
+    <meta http-equiv="Expires" content="0" />
     <meta property="og:type" content="website" />
     <meta property="og:locale" content="id" />
     <meta property="og:site_name" content="Gerakan Nasional Sejuta Surya Atap" />
