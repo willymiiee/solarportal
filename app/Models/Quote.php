e5ce9ea34fb7cc8f831bfb2fae947b47b52ee4dc
Willy Nugraha <willymiiee@gmail.com>
2018-07-24 20:47:23 +0800

    add user relation with quote

--- app/Models/Quote.php
+++ app/Models/Quote.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
 
 class Quote extends Model
 {
@@ -43,4 +44,9 @@ class Quote extends Model
     protected $dates = [
         'created_at', 'updated_at'
     ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
